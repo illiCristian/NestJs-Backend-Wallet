85d9c31441b844bf202a4f191a4145f7abd100eb
Adri-ESM <anggivmorales@gmail.com>
2023-08-23 12:33:21 -0400

    cards on banner

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,25 +1,17 @@
-<<<<<<< HEAD
 import { Navbar } from '@/components'
 import Image from 'next/image'
-=======
-import Footer from "./pages/footer/page";
-import SecondCard from "./pages/secondCard/page";
->>>>>>> e1ba4f61554d09337b2229405184673bd5c22a6b
+import Footer from './pages/footer/page'
+//import SecondCard from './pages/secondCard/page'
 
 const Page = (): JSX.Element => {
   return (
     <div>
-<<<<<<< HEAD
       <Navbar />
       <main></main>
+      <Footer />
+      {/* <SecondCard /> */}
     </div>
   )
-=======
-      <Footer />
-      <SecondCard />
-    </div>
-  );
->>>>>>> e1ba4f61554d09337b2229405184673bd5c22a6b
 }
 
 export default Page
--- client/src/components/navbar-banner/Banner.tsx
+++ client/src/components/navbar-banner/Banner.tsx
@@ -2,6 +2,8 @@ import './styles.css'
 import Image from 'next/image'
 import BannerImg from '../../../public/images/banner6.jpeg'
 import BannerImg2 from '../../../public/images/imagen-banner.png'
+import Vector from '../../../public/vectors/vector.png'
+import Vector1 from '../../../public/vectors/vector1.png'
 
 export default function Banner () {
   return (
@@ -20,7 +22,7 @@ export default function Banner () {
           </div>
           <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
             <Image
-              className='banner-image2 lg:w-2/6 md:w-3/6 w-5/6 object-cover object-center rounded'
+              className='banner-image2 lg:w-2/6 md:w-3/6 w-5/6 object-cover object-center rounded '
               alt='hero'
               src={BannerImg2}
               width={720}
@@ -29,56 +31,66 @@ export default function Banner () {
           </div>
         </div>
       </section>
-      <section>
-        <div className='container mx-auto flex px-5 py-24 items-center justify-center flex-col'>
-          {/* <Image
-            className='lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded'
-            alt='hero'
-            src='https://dummyimage.com/720x600'
-            width={720}
-            height={600}
-          /> */}
-        </div>
-        <div>
-          <h1>Hola</h1>
-        </div>
-      </section>
-      <section>
-        <div>
-          <div className='container mx-auto flex px-5 py-24 items-center justify-center flex-col'>
-            {/* <Image
-              className='lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded'
-              alt='hero'
-              src='https://dummyimage.com/720x600'
-              width={720}
-              height={600}
-            /> */}
+      <div className='flex'>
+        <section>
+          <div className='flex'>
+            <div className='container mx-auto flex px-5 py-24 items-center justify-center flex-col'>
+              <div className='card flex items-center'>
+                <Image
+                  className='card-vector lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded'
+                  alt='hero'
+                  src={Vector1}
+                  width={200}
+                  height={100}
+                />
+                <div className='text-and-button'>
+                  <div>
+                    <h2 className='w-full'>
+                      Maneja tus finanzas, y transfiere gratis
+                    </h2>
+                    <p className='w-full'>
+                      Puedes hacer crecer tus ahorros con GBM, comprar en meses
+                      y pagar servicios desde el celular.
+                    </p>
+                  </div>
+                  <div>
+                    <button className='w-full'>Conoce tus cuentas</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <h2>Hola</h2>
+        </section>
+        <section>
+          <div className='flex'>
+            <div className='container mx-auto flex px-5 py-24 items-center justify-center flex-col'>
+              <div className='card flex items-center'>
+                <Image
+                  className='card-vector lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded'
+                  alt='hero'
+                  src={Vector}
+                  width={200}
+                  height={100}
+                />
+                <div className='text-and-button'>
+                  <div>
+                    <h2 className='w-full'>
+                      Maneja tus finanzas, y transfiere gratis
+                    </h2>
+                    <p className='w-full'>
+                      Puedes hacer crecer tus ahorros con GBM, comprar en meses
+                      y pagar servicios desde el celular.
+                    </p>
+                  </div>
+                  <div>
+                    <button className='w-full'>Conoce tus cuentas</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <button>Click me</button>
-          </div>
-        </div>
-        <div>
-          <div className='container mx-auto flex px-5 py-24 items-center justify-center flex-col'>
-            {/* <Image
-              className='lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded'
-              alt='hero'
-              src='https://dummyimage.com/720x600'
-              width={720}
-              height={600}
-            /> */}
-          </div>
-          <div>
-            <h2>Hola</h2>
-          </div>
-          <div>
-            <button>Click me</button>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   )
 }
