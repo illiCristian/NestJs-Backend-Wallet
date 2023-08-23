17f36c72ac4e806fa4713d52133a8356d06ff9f9
Adri-ESM <anggivmorales@gmail.com>
2023-08-23 11:35:09 -0400

    working with nabvar modals and banner components

--- client/src/components/navbar-banner/Banner.tsx
+++ client/src/components/navbar-banner/Banner.tsx
@@ -1,16 +1,16 @@
 import './styles.css'
 import Image from 'next/image'
 import BannerImg from '../../../public/images/banner6.jpeg'
-import BannerImg2 from '../../../public/images/image-banner.jpeg'
+import BannerImg2 from '../../../public/images/imagen-banner.png'
 
 export default function Banner () {
   return (
     <div>
-      <section className='banner'>
-        <div className='container max-w-full px-0 py-0 items-center justify-center flex-col'>
-          <div className='w-full opacity-80'>
+      <section className='banner '>
+        <div className='container max-w-full px-0 py-0 items-center justify-center flex-col '>
+          <div className='w-full opacity-80 '>
             <Image
-              className='banner-image w-full object-cover object-center rounded opacity-80'
+              className='banner-image w-full object-cover object-center opacity-80 relative'
               alt='hero'
               src={BannerImg}
               layout='responsive'
@@ -18,13 +18,15 @@ export default function Banner () {
               height={100}
             />
           </div>
-          <Image
-            className='lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded'
-            alt='hero'
-            src={BannerImg2}
-            width={720}
-            height={600}
-          />
+          <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+            <Image
+              className='banner-image2 lg:w-2/6 md:w-3/6 w-5/6 object-cover object-center rounded'
+              alt='hero'
+              src={BannerImg2}
+              width={720}
+              height={600}
+            />
+          </div>
         </div>
       </section>
       <section>
