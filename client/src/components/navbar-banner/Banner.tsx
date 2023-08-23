6b44f9911b5ba80dbad8d0e943e510c1d3a415e3
Adri-ESM <anggivmorales@gmail.com>
2023-08-23 14:55:22 -0400

    cards on banner

--- client/src/components/navbar-banner/Banner.tsx
+++ client/src/components/navbar-banner/Banner.tsx
@@ -8,7 +8,7 @@ import Vector1 from '../../../public/vectors/vector1.png'
 export default function Banner () {
   return (
     <div>
-      <section className='banner '>
+      <section className='banner relative'>
         <div className='container max-w-full px-0 py-0 items-center justify-center flex-col '>
           <div className='w-full opacity-80 '>
             <Image
@@ -22,7 +22,7 @@ export default function Banner () {
           </div>
           <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
             <Image
-              className='banner-image2 lg:w-2/6 md:w-3/6 w-5/6 object-cover object-center rounded '
+              className='banner-image2 object-cover object-center rounded'
               alt='hero'
               src={BannerImg2}
               width={720}
@@ -32,9 +32,9 @@ export default function Banner () {
         </div>
       </section>
       <div className='flex'>
-        <section>
-          <div className='flex'>
-            <div className='container mx-auto flex px-5 py-24 items-center justify-center flex-col'>
+        <section className='flex-1'>
+          <div className='container mx-auto flex px-5 py-24 items-center justify-center flex-col'>
+            <div className='flex space-x-4'>
               <div className='card flex items-center'>
                 <Image
                   className='card-vector lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded'
@@ -63,32 +63,28 @@ export default function Banner () {
             </div>
           </div>
         </section>
-        <section>
-          <div className='flex'>
-            <div className='container mx-auto flex px-5 py-24 items-center justify-center flex-col'>
-              <div className='card flex items-center'>
-                <Image
-                  className='card-vector lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded'
-                  alt='hero'
-                  src={Vector}
-                  width={200}
-                  height={100}
-                />
-                <div className='text-and-button'>
-                  <div>
-                    <h2 className='w-full'>
-                      Vende y ten tu dinero al instante
-                    </h2>
-                    <p className='w-full'>
-                      Cobra con los medios de pago más usados y accede a un
-                      crédito sin complicaciones.
-                    </p>
-                  </div>
-                  <div>
-                    <button className='w-full bg-tertiary text-white hover:bg-tertiaryDark'>
-                      Conoce tus cuentas
-                    </button>
-                  </div>
+        <section className='flex-1'>
+          <div className='container mx-auto flex px-5 py-24 items-center justify-center flex-col'>
+            <div className='card flex items-center'>
+              <Image
+                className='card-vector lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded'
+                alt='hero'
+                src={Vector}
+                width={200}
+                height={100}
+              />
+              <div className='text-and-button'>
+                <div>
+                  <h2 className='w-full'>Vende y ten tu dinero al instante</h2>
+                  <p className='w-full'>
+                    Cobra con los medios de pago más usados y accede a un
+                    crédito sin complicaciones.
+                  </p>
+                </div>
+                <div>
+                  <button className='w-full bg-tertiary text-white hover:bg-tertiaryDark'>
+                    Conoce tus cuentas
+                  </button>
                 </div>
               </div>
             </div>
