8ae01ba5131ca9261395f93b98845f2be7704980
Adri-ESM <anggivmorales@gmail.com>
2023-08-31 23:38:50 -0400

    transfer debit styles

--- client/src/Provider/TransferCards.tsx
+++ client/src/Provider/TransferCards.tsx
@@ -1,22 +1,22 @@
-import React, { createContext, useState } from 'react'
+// import React, { createContext, useState } from 'react'
 
-export const TransferCardsContext = createContext({})
+// export const TransferCardsContext = createContext({})
 
-export const TransferCardsProvider = ({ children }) => {
-  const [numberOperationTransferDebit, setNumberOperationTransferDebit] =
-    useState('')
-  const [cardTypeTransfer, setCardTypeTransfer] = useState('')
+// export const TransferCardsProvider = ({ children }) => {
+//   const [numberOperationTransferDebit, setNumberOperationTransferDebit] =
+//     useState('')
+//   const [cardTypeTransfer, setCardTypeTransfer] = useState('')
 
-  const contextValue = {
-    numberOperationTransferDebit,
-    setNumberOperationTransferDebit,
-    cardTypeTransfer,
-    setCardTypeTransfer
-  }
+//   const contextValue = {
+//     numberOperationTransferDebit,
+//     setNumberOperationTransferDebit,
+//     cardTypeTransfer,
+//     setCardTypeTransfer
+//   }
 
-  return (
-    <TransferCardsContext.Provider value={contextValue}>
-      {children}
-    </TransferCardsContext.Provider>
-  )
-}
+//   return (
+//     <TransferCardsContext.Provider value={contextValue}>
+//       {children}
+//     </TransferCardsContext.Provider>
+//   )
+// }
