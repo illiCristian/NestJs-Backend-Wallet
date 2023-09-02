0c9d5239bf3f54adf46290130d43129d072e4680
Adri-ESM <anggivmorales@gmail.com>
2023-09-01 22:41:45 -0400

    creatin profile page on home

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Navbar } from '@/components'
 import Providers from './providers'
 import Footer from '../components/LandingPage/Footer/Footer'
-import Profile from '@/components/Profile/Profile'
+import Profile from '@/app/home/profile/page'
 
 const Page = (): JSX.Element => {
   return (
