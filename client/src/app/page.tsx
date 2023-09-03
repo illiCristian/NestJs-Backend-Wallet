c1c3221613e3ac2b82a3604fe2339c62861e4536
Adri-ESM <anggivmorales@gmail.com>
2023-09-02 22:51:21 -0400

    finished profile

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,16 +1,13 @@
 import { Navbar } from '@/components'
 import Providers from './providers'
 import Footer from '../components/LandingPage/Footer/Footer'
-import Profile from '@/app/home/profile/page'
 
 const Page = (): JSX.Element => {
   return (
     <div>
       <Providers>
         <Navbar />
-        <main>
-          <Profile />
-        </main>
+        <main></main>
         <Footer />
       </Providers>
     </div>
