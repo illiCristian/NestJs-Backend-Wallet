29d24f17cb38019f0337c85468a80c30e7948684
“agtelo” <alantelo1987@gmail.com>
2023-09-06 18:16:17 -0300

    feat: add spei route and compoenent

--- client/src/components/transferDebit/transferOptions/TransferOptions.tsx
+++ client/src/components/transferDebit/transferOptions/TransferOptions.tsx
@@ -3,6 +3,7 @@ import credit_icon from '../../../../public/credit-card-icon.svg'
 import next_arrow from '../../../../public/next-arrow-icon.svg'
 import { MouseEventHandler } from 'react'
 import DetailMoney from '../DetailMoney/DetailMoney'
+import Link from 'next/link'
 
 interface props {
   component: string
@@ -47,7 +48,10 @@ const TransferOptions = ({ component, nextStep }: props) => {
             />
           </div>
           <hr className=" bg-gray-200 border-1" />
-          <div className="flex flex-row justify-between  items-start my-4 px-4">
+          <Link
+            href={'/spei'}
+            className="flex flex-row justify-between  items-start my-4 px-4"
+          >
             <div className="flex ">
               <div className="border rounded-full border-gray-300 mx-2">
                 <Image
@@ -71,9 +75,8 @@ const TransferOptions = ({ component, nextStep }: props) => {
               width={25}
               height={25}
               className=" cursor-pointer"
-              onClick={nextStep}
             />
-          </div>
+          </Link>
         </div>
         <DetailMoney />
       </section>
