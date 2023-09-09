8cef414ad2699838d359a02c422ecf450e56b4cb
“agtelo” <alantelo1987@gmail.com>
2023-09-09 02:37:10 -0300

    fix: styles in card component

--- client/src/components/SendMoney/SelectMethod/Card.tsx
+++ client/src/components/SendMoney/SelectMethod/Card.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import add from '../../../../public/assets/send-money/Add.svg'
-import logo from '../../../../public/assets/send-money/logo.svg'
+import logo2 from '../../../../public/assets/send-money/logo2.svg'
 import aton from '../../../../public/assets/send-money/aton.svg'
 import Image from 'next/image'
 
 export default function Card() {
   return (
     <>
-      <div className="w-[420.21px] h-[199px] bg-neutral-500 rounded-[10px] relative">
-        <div className="flex justify-between p-4">
-          <Image src={logo} alt="logo" className="" />
-          <div className="flex gap-4 items-center ">
+      <button className="w-[420.21px] h-[199px] focus:grayscale-0 focus:outline focus:outline-2 focus:outline-blue-500 bg-tertiary grayscale rounded-[10px] relative">
+        <div className="flex justify-between p-4 ">
+          <Image
+            src={logo2}
+            alt="logo"
+            width={64}
+            height={64}
+            className="mb-8"
+          />
+          <div className="flex gap-4 mb-14 items-center">
             <p className="text-white"> $6000</p>
-            <Image src={aton} alt="eye" />
+            <Image src={aton} alt="eye" className="" />
           </div>
         </div>
 
@@ -27,7 +33,7 @@ export default function Card() {
             PAGO
           </div>
         </div>
-        <div className="flex justify-between mt-14">
+        <div className="flex justify-between mt-6 mb-2 ">
           <div className="w-28 text-white text-base font-normal ms-7">
             Sin comisión
           </div>
@@ -37,7 +43,7 @@ export default function Card() {
             </div>
           </div>
         </div>
-      </div>
+      </button>
     </>
   )
 }
